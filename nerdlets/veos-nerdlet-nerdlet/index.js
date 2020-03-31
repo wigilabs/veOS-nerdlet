8ee5ea8822847b445f053058a20f5a447ce303df
Dieter Evangelista <hansevangelista@gmail.com>
2020-03-31 14:27:11 -0500

    first commit

---
+++ nerdlets/veos-nerdlet-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class VeosNerdletNerdletNerdlet extends React.Component {
+    render() {
+        return <h1>Hello, veos-nerdlet-nerdlet Nerdlet!</h1>;
+    }
+}
